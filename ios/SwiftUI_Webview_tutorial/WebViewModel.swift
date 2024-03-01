a03bd157ec21949d2cebe9f1210acda55409039a
wooooooongs <pires11233@gmail.com>
2024-03-01 16:49:30 +0900

    :sparkles: WebViewModel 구현 및 contentView에 공유

---
+++ ios/SwiftUI_Webview_tutorial/WebViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  WebViewModel.swift
+//  SwiftUI_Webview_tutorial
+//
+//  Created by Oscar on 3/1/24.
+//  Copyright © 2024 Tuentuenna. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+// 별칭
+typealias WEB_NAVIGATION = WebViewModel.Navigation
+
+class WebViewModel: ObservableObject {
+    enum Navigation {
+        case back, refresh, forward
+    }
+    
+    enum URL_TYPE {
+        case naver
+        case google
+        case main
+        
+        var url: URL? {
+            switch self {
+            case .naver:
+                return URL(string: "https://www.naver.com")
+            case .google:
+                return URL(string: "https://www.google.com")
+            case .main:
+                return URL(string: "https://wooooooongs.github.io/webview_test/")
+            }
+        }
+    }
+    
+    // 웹뷰의 URL이 변경되었을 때 이벤트 전달해줌.
+    var changedURLSubject = PassthroughSubject<WebViewModel.URL_TYPE, Never>()
+}
